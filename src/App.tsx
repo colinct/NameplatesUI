f8bef487644d73b54f673367e6561031802aaab9
Colin Tompkins <colin@tyrannosaurustech.com>
2024-08-03 19:55:11 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        "press escape to open menu"
+      </div>
+    </>
+  )
+}
+
+export default App
